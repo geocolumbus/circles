f785a11c77b04e4ab151dabf436e4ca5abbd2807
geocolumbus <geocolumbus@gmail.com>
2013-08-13 07:52:30 -0400

    first commit

---
+++ circles/GC_view.h
@@ -0,0 +1,13 @@
+//
+//  GC_view.h
+//  circles
+//
+//  Created by George Campbell on 8/11/13.
+//  Copyright (c) 2013 george. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GC_view : UIView
+
+@end
---
+++ circles/GC_view.m
@@ -0,0 +1,31 @@
+//
+//  GC_view.m
+//  circles
+//
+//  Created by George Campbell on 8/11/13.
+//  Copyright (c) 2013 george. All rights reserved.
+//
+
+#import "GC_view.h"
+
+@implementation GC_view
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
